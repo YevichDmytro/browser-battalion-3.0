3f61c634d34b0bdc86f6854230087c21785a79b5
Nikitin <nikitinigor0907@gmail.com>
2024-10-16 21:14:17 +0200

    router-logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
+import "modern-normalize";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.jsx";
-import "modern-normalize";
 import "./index.css";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
