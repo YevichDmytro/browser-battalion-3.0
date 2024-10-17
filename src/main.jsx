0bfdeb6330212942cde0946bf21038801485a346
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-17 20:26:43 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.jsx";
 import "./index.css";
 import { store } from "./redux/store";
