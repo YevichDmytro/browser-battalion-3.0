b2365977bdb7b53a1c4b3a340061b8d0c655798b
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-18 15:12:07 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,1 @@
-import Logo from "../Logo/Logo";
-import Container from "../ui/Container/Container";
-import UserAuth from "../UserAuth/UserAuth";
 
-const Header = ({ isAuthenticated }) => {
-  return (
-    <header>
-      <Container>
-        <Logo isAuthenticated={isAuthenticated} />
-        <UserAuth />
-      </Container>
-    </header>
-  );
-};
-
-export default Header;
