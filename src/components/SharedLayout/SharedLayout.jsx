d057682f687ffe4a790bc312d16f4a503827e415
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-18 15:14:00 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,1 @@
-import css from "./SharedLayout.module.css";
 
-import { Outlet } from "react-router-dom";
-
-import Header from "../Header/Header";
-
-const SharedLayout = ({ isAuthenticated }) => {
-  return (
-    <div className={css.layout}>
-      <Header isAuthenticated={isAuthenticated} />
-      <Outlet />
-    </div>
-  );
-};
-
-export default SharedLayout;
