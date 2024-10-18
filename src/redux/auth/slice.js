4e87908ba162517812db7bf601a499f2db58751c
Nataliia21a <152820534+Nataliia21a@users.noreply.github.com>
2024-10-18 18:00:15 +0300

    redux auth register

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { register } from "./operations";
 
 const authInitialState = {
   user: {
@@ -16,9 +17,26 @@ const authInitialState = {
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState: authInitialState,
-  // extraReducers: builder => builder.addCase(),
+  extraReducers: (builder) =>
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        // state.token = action.payload.token;
+        state.isAuthenticated = true;
+        state.loading = false;
+        state.error = false;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isAuthenticated = false;
+        state.loading = false;
+        state.error = true;
+      }),
 });
 
 export const authReducer = authSlice.reducer;
