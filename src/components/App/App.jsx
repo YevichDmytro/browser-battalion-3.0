b9b0ec7eac5482e6f31c192bad7d1454004ada19
Vikto0004 <vikto0004@gmail.com>
2024-10-18 17:30:15 +0200

    register page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import SharedLayout from '../SharedLayout/SharedLayout'
+import SharedLayout from "../SharedLayout/SharedLayout";
 
 const HomePage = lazy(() => import("../../pages/HomePage"));
 const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
