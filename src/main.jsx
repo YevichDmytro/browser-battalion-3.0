de14a8cb57bdd704f8701030765ad7b8a354be47
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-20 19:50:00 +0300

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import "modern-normalize";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./components/App/App.jsx";
-import { PersistGate } from "redux-persist/integration/react";
-import { store } from './redux/store';
+import 'modern-normalize';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import "modern-normalize";
-import "./reset.css";
-import "./global.css";
-import { store, persistor } from "./redux/store";
+import App from './components/App/App.jsx';
+import 'modern-normalize';
+import './reset.css';
+import './global.css';
+import { store, persistor } from './redux/store';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
