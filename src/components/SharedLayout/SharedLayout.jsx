8fbe37676b4fbef92c8dccb484a0c895995f7318
Varu <142126351+olenarokalo@users.noreply.github.com>
2024-10-20 11:31:03 +0100

    update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,14 +4,11 @@ import css from "./SharedLayout.module.css";
 
 import Header from "../Header/Header";
 
-import Loader from "../ui/Loader/Loader";
-
 const SharedLayout = () => {
   return (
     <div className={css.layout}>
       <Header />
       <Outlet />
-      <Loader />
     </div>
   );
 };
