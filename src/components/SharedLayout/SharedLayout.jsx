6f4a1f547cd08b527be666543fa52404ad9300bc
Varu <142126351+olenarokalo@users.noreply.github.com>
2024-10-20 11:30:04 +0100

    chanded-fonts

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 
 import css from "./SharedLayout.module.css";
+
 import Header from "../Header/Header";
+
+import Loader from "../ui/Loader/Loader";
 
 const SharedLayout = () => {
   return (
     <div className={css.layout}>
       <Header />
       <Outlet />
+      <Loader />
     </div>
   );
 };
