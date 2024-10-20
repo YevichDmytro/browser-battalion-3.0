141c04a9270b918f41fa053eabdfa71049d11c51
Oleksandr <olexandr.veseluy@gmail.com>
2024-10-20 16:41:13 +0300

    update btn-component

--- src/components/WaterConsumptionTracker/WaterСonsumptionTracker.jsx
+++ src/components/WaterConsumptionTracker/WaterСonsumptionTracker.jsx
@@ -1,52 +1,59 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-import css from './WaterConsumptionTracker.module.css'
-import icons from '../../assets/welcome-page/icons.svg'
-import Button from '../ui/Button/Button'
-
+import css from './WaterConsumptionTracker.module.css';
+import icons from '../../assets/welcome-page/icons.svg';
+import Button from '../ui/Button/Button';
 
 const WaterConsumptionTracker = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const tryTrackerOnClick = () => {
+    navigate('/signup');
+  };
 
-    const tryTrackerOnClick = () => {
-        navigate('/signup')
-    }
+  return (
+    <div className={css.justForCheck}>
+      <div className={css.titleBox}>
+        <h2 className={css.mainTitle}>Water consumption tracker</h2>
+        <h3 className={css.titleDescription}>
+          Record daily water intake and track
+        </h3>
+      </div>
 
-    return (
-        <div className={css.justForCheck}>
-            <div className={css.titleBox}>
-                <h2 className={css.mainTitle}>Water consumption tracker</h2>
-                <h3 className={css.titleDescription}>Record daily water intake and track</h3>
-            </div>
+      <div className={css.benefitsBox}>
+        <h4 className={css.benefitsTitle}>Tracker Benefits</h4>
+        <ul className={css.list}>
+          <li className={css.listItem}>
+            <svg className={css.listItemSvg} height={32} width={32}>
+              <use href={`${icons}#icon-key`}></use>
+            </svg>
+            Habit drive
+          </li>
+          <li className={css.listItem}>
+            <svg className={css.listItemSvg} height={32} width={32}>
+              <use href={`${icons}#icon-statistic`}></use>
+            </svg>
+            Viev statistics
+          </li>
+          <li className={css.listItem}>
+            <svg className={css.listItemSvg} height={32} width={32}>
+              <use href={`${icons}#icon-calendar`}></use>
+            </svg>
+            Personal rate setting
+          </li>
+        </ul>
+      </div>
 
-            <div className={css.benefitsBox}>
-                <h4 className={css.benefitsTitle}>Tracker Benefits</h4>
-                <ul className={css.list}>
-                    <li className={css.listItem}>
-                        <svg className={css.listItemSvg} height={32} width={32}>
-                            <use href={`${icons}#icon-key`}></use>
-                        </svg>
-                        Habit drive
-                    </li>
-                    <li className={css.listItem}>
-                        <svg className={css.listItemSvg} height={32} width={32}>
-                            <use href={`${icons}#icon-statistic`}></use>
-                        </svg>
-                        Viev statistics
-                    </li>
-                    <li className={css.listItem}>
-                        <svg className={css.listItemSvg} height={32} width={32}>
-                            <use href={`${icons}#icon-calendar`}></use>
-                        </svg>
-                        Personal rate setting
-                    </li>
-                </ul>
-            </div>
-
-            <Button onClick={tryTrackerOnClick} className={css.trackerButton}>Try tracker</Button>
-        </div>
-    )
-}
+      <Button
+        onClick={tryTrackerOnClick}
+        className={css.trackerButton}
+        variant="link"
+        href="/signin"
+      >
+        Try tracker
+      </Button>
+    </div>
+  );
+};
 
 export default WaterConsumptionTracker;
