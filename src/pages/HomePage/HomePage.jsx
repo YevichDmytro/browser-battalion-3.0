1019113791b973dda77687e824083434cfcac6a6
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-24 20:17:05 +0300

    Revert "add homepage markup"

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,12 @@
-import css from './HomePage.module.css';
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 import Container from '../../components/ui/Container/Container';
 
 const HomePage = () => {
   return (
-    <Container className={css.homepage}>
-      <div className={css.col}>
-        <DailyNorma />
-      </div>
-      <div className={css.col}>
-        <TodayWaterList />
-      </div>
+    <Container>
+      <DailyNorma />
+      <TodayWaterList />
     </Container>
   );
 };
