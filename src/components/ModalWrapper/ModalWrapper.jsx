505b88534bf10bfd039d46882c4ea95e530cde25
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-10-25 17:38:15 +0300

    changes in wrapper and settings modal

--- src/components/ModalWrapper/ModalWrapper.jsx
+++ src/components/ModalWrapper/ModalWrapper.jsx
@@ -5,7 +5,7 @@ import css from './ModalWrapper.module.css';
 
 Modal.setAppElement('#root');
 
-const ModalWrapper = ({ isOpen, onClose, children }) => {
+const ModalWrapper = ({ isOpen, onClose, children, modalType }) => {
   useEffect(() => {
     document.body.style.overflow = isOpen ? 'hidden' : 'unset';
 
@@ -19,13 +19,17 @@ const ModalWrapper = ({ isOpen, onClose, children }) => {
       isOpen={isOpen}
       onRequestClose={onClose}
       shouldCloseOnOverlayClick={true}
-      className={css.modal}
-      overlayClassName={css.backdrop}
+      className={modalType === 'transparent' ? '' : css.defaultModal}
+      overlayClassName={
+        modalType === 'transparent'
+          ? css.transparentOverlay
+          : css.defaultOverlay
+      }
     >
       <div className={css.modalContent}>
         <button className={css.closeButton} onClick={onClose}>
           <svg width="16" height="16" className={css.iconClose}>
-            <use href={`/src/assets/logout/x-btn.svg#x-btn`}></use>
+            <use href={`/public/logout/x-btn.svg#x-btn`}></use>
           </svg>
         </button>
         {children}
--- utils/userInfoValidationSchema.js
+++ utils/userInfoValidationSchema.js
@@ -41,38 +41,23 @@ export const userInfoValidationSchema = Yup.object({
       }
       return true;
     }),
-  password: Yup.string()
-    .min(8, 'Too short')
-    .max(64, 'Too long')
-    .test('password-filled', 'Required', function (value) {
-      const { outdatedPassword, repeatPassword } = this.parent;
-      if ((outdatedPassword || repeatPassword) && !value) {
-        return false;
-      }
-      return true;
-    }),
+  password: Yup.string().min(8, 'Too short').max(64, 'Too long').nullable(), // Make it nullable to allow empty values
   repeatPassword: Yup.string()
     .oneOf([Yup.ref('password')], 'Passwords must match')
-    .test('repeat-password-filled', 'Required', function (value) {
-      const { outdatedPassword, password } = this.parent;
-      if ((outdatedPassword || password) && !value) {
-        return false;
-      }
-      return true;
-    }),
+    .nullable(),
 });
 
 export const WaterRateValidationSchema = Yup.object({
   waterRate: Yup.string()
-    .required("Water rate is required")
-    .min(1, "Please, add you water rate!")
-    .max(15000, "You drank too much water!!"),
+    .required('Water rate is required')
+    .min(1, 'Please, add you water rate!')
+    .max(15000, 'You drank too much water!!'),
 });
 
 export const WaterNotesValidationSchema = Yup.object({
   waterVolume: Yup.string()
-    .required("Water volume is required")
-    .min(1, "Please, add you water volume!")
-    .max(15000, "You drank too much water!!"),
-  date: Yup.string().required("Date is required").typeError("Incorrect date"),
+    .required('Water volume is required')
+    .min(1, 'Please, add you water volume!')
+    .max(15000, 'You drank too much water!!'),
+  date: Yup.string().required('Date is required').typeError('Incorrect date'),
 });
