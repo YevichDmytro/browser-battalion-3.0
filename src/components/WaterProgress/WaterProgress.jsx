e56e007c0c665594c1af317727f779f14646dba3
Den4ik382 <denlikhman1@gmail.com>
2024-10-25 21:25:18 +0200

    2

--- src/components/WaterProgress/WaterProgress.jsx
+++ src/components/WaterProgress/WaterProgress.jsx
@@ -3,21 +3,33 @@ const WaterProgress = ({ currentWater, waterGoal }) => {
   const progress = (currentWater / waterGoal) * 100;
 
   return (
-    <div className={css.waterProgress}>
-      <div className={css.waterProgressBar} style={{ width: `${progress}%` }}>
-        {currentWater} / {waterGoal} ml
+    <div className={css.waterProgressContainer}>
+      <div className={css.waterProgress}>
+        <div className={css.waterProgressBar} style={{ width: `${progress}%` }}>
+          <svg
+            className={css.progressIcon}
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="7" cy="7" r="6.5" fill="white" stroke="#407BFF" />
+          </svg>
+        </div>
       </div>
       <div className={css.block}>
         <div className={css.blocks}>
-          <div className={css.block1}>|</div>
+          <div className={css.block1}></div>
+
           <div>0%</div>
         </div>
         <div className={css.blocks}>
-          <div className={css.block2}>|</div>
+          <div className={css.block1}></div>
           <div>50%</div>
         </div>
         <div className={css.blocks}>
-          <div className={css.block3}>|</div>
+          <div className={css.block1}></div>
           <div>100%</div>
         </div>
       </div>
--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import TodayListModal from './TodayListModal';
-import WaterProgress from '../WaterProgress/WaterProgress';
 import css from './WaterRatioPanel.module.css';
+import WaterProgress from '../WaterProgress/WaterProgress';
+
 const WaterRatioPanel = () => {
   const waterGoal = 2000;
   // const [currentWater, setCurrentWater] = useState(0);
   // const [isModalOpen, setModalOpen] = useState(false);
-  const currentWater = 1500;
+  const currentWater = 1000;
   return (
     <div className={css.waterPanel}>
-      <h2 className={css.today}>Today</h2>
-      <WaterProgress currentWater={currentWater} waterGoal={waterGoal} />
+      <div className={css.waterProgressBar}>
+        <h2 className={css.today}>Today</h2>
+        <WaterProgress currentWater={currentWater} waterGoal={waterGoal} />
+      </div>
       <button className={css.btnAddWater}>Add Water</button>
       {/* {isModalOpen && (
         <TodayListModal />
