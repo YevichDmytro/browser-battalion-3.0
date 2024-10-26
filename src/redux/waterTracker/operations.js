de36be6f52ca679b270dc71c04cf36d0414ea96f
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-26 19:16:57 +0300

    add deploy urls

--- src/redux/waterTracker/operations.js
+++ src/redux/waterTracker/operations.js
@@ -1,8 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  'https://browser-battalion-3-0-backend-kyxl.onrender.com';
+axios.defaults.baseURL = 'https://browser-battalion-3-0-backend.onrender.com';
 
 export const addWaterItem = createAsyncThunk(
   'water/addItem',
